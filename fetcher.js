c500c36b7ee357276a653cbac05042c4b780071b
Carmen Tsang <carmtsang89@hotmail.com>
2022-04-23 01:50:08 +0000

    created index.html, made HTTP request and got data to write to index.html file using fs

--- fetcher.js
+++ fetcher.js
@@ -1,12 +1,22 @@
 const request = require('request');
+const fs = require('fs');
 
+//command line takes in 2 arguments - a url (url) and local file path (localPath)
 const args = process.argv.slice(2);
 const url = args[0];
 const localPath = args[1]
 
+//download the resources at the URL to the local path
+//upon completion, it should print out a message
 
 request(url, (error, response, body) => {
   console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
+  fs.writeFile(localPath, body, err => {
+    if (err) {
+      console.log(err)
+    }
+  })
+
+  //print out message to terminal
+  //console.log(`Downloaded and saved ${data} bytes to ${localPath}`)
 });
