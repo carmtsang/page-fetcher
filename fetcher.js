eaaa26e97785b7aaee38883d8c4f2deaf0872bfe
Carmen Tsang <carmtsang89@hotmail.com>
2022-04-23 02:37:27 +0000

    lint test completed

--- fetcher.js
+++ fetcher.js
@@ -4,30 +4,28 @@ const fs = require('fs');
 //command line takes in 2 arguments - a url (url) and local file path (localPath)
 const args = process.argv.slice(2);
 const url = args[0];
-const localPath = args[1]
+const localPath = args[1];
 
 //download the resources at the URL to the local path
 //upon completion, it should print out a message
 
 request(url, (error, response, body) => {
-  
-  
-  fs.writeFile(localPath, body, err => {
+  if (error) {
+    console.log('error:', error); //only print error if there is one.
+  }
+
+  fs.writeFile(localPath, body, err => { //write url to index.html
     if (err) {
-      console.log(err)
+      console.log(err);
       return;
     }
     fs.stat(localPath, (err, stats) => { //get file size in bytes
       if (err) {
-        console.log(err)
+        console.log(err);
         return;
       }
-      console.log(`Downloaded and saved ${stats.size} bytes to ${localPath}`)
+      console.log(`Downloaded and saved ${stats.size} bytes to ${localPath}`);
     });
   });
 
-
-
-
-  //print out message to terminal
 });
